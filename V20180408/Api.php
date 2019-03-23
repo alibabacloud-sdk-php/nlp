087847d48877abab6a45eaf44e8ffe3cc7febdf3
顾咏 <zhijiang.nzj@alibaba-inc.com>
2019-03-23 17:03:28 +0800

    Update Apis

--- V20180408/Api.php
+++ V20180408/Api.php
@@ -2,7 +2,26 @@
 
 namespace AlibabaCloud\Nlp\V20180408;
 
+use AlibabaCloud\ApiResolverTrait;
 use AlibabaCloud\Roa;
+
+/**
+ * Resolve Api based on the method name.
+ *
+ * @method IE iE(array $options = [])
+ * @method TextStructure textStructure(array $options = [])
+ * @method KWE kWE(array $options = [])
+ * @method Translate translate(array $options = [])
+ * @method Sentiment sentiment(array $options = [])
+ * @method WordPos wordPos(array $options = [])
+ * @method Entity entity(array $options = [])
+ * @method WordSegment wordSegment(array $options = [])
+ * @method ReviewAnalysis reviewAnalysis(array $options = [])
+ */
+class NlpApiResolver
+{
+    use ApiResolverTrait;
+}
 
 class V20180408Roa extends Roa
 {
